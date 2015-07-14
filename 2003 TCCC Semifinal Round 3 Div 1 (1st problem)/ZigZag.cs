7b3be721977b9e237704c0794ea03b8fed7f9690
Matthias <matthias@orangium.com>
2015-07-14 12:27:59 -0400

    Two new TC problems solved

---
+++ 2003 TCCC Semifinal Round 3 Div 1 (1st problem)/ZigZag.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZigZag
+{
+    public class ZigZag
+    {
+        public int longestZigZag(int[] sequence)
+        {
+            bool[] positive = new bool[sequence.Length];
+            int[] longest = new int[sequence.Length];
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                if (i == 0)
+                {
+                    longest[i] = 1;
+                }
+
+                if (i == 1)
+                {
+                    longest[i] = 2;
+                    positive[i] = (sequence[i] - sequence[i - 1] > 0);
+                }
+
+                if (i > 1)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (j == 0)
+                        {
+                            positive[i] = (sequence[i] - sequence[j] > 0);
+                            longest[i] = 2;
+                        }
+                        else if (((positive[j] && sequence[i] - sequence[j] < 0) || (!positive[j] && sequence[i] - sequence[j] > 0)) && (longest[j] + 1 > longest[i]))
+                        {
+                            positive[i] = (sequence[i] - sequence[j] > 0);
+                            longest[i] = longest[j] + 1;
+                        }
+                    }
+                }
+            }
+
+            int result = longest[0];
+            for (int i = 0; i < longest.Length; i++)
+            {
+                if (longest[i] > result) result = longest[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ 2004 TCCC Online Round 4 Div 1 (1st problem)/BadNeighbors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadNeighbors
+{
+    class BadNeighbors
+    {
+        public int maxDonations(int[] donations)
+        {
+            // Find max1 between i=0 and i=length-2
+            // Find max2 between i=1 and i=length-1
+            // Return max of max1 max2
+            return Math.Max(getMax(donations, 0, donations.Length - 2), getMax(donations, 1, donations.Length - 1));
+        }
+
+        public int getMax(int[] donations, int from, int to)
+        {
+            int max = 0;
+            int[] maxForI = new int[donations.Length];
+            for (int i = from; i <= to; i++)
+            {
+                maxForI[i] = donations[i];
+                for (int j = 0; j < i - 1; j++)
+                {
+                    maxForI[i] = Math.Max(maxForI[i], maxForI[j] + donations[i]);
+                }
+                max = Math.Max(max, maxForI[i]);
+            }
+            return max;
+        }
+    }
+}
